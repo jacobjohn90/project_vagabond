f7729392474891f179f61e1e2d03ae80a803454a
JavaGirl18 <valenciacooper89@gmail.com>
2018-07-16 18:01:25 -0400

    updated slideshow

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,23 +7,23 @@ class HomePage extends Component {
             <div>
                 <Carousel>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="https://source.unsplash.com/J-ClkL7wNUs/900x500" />
+    <img width={13000} height={100} alt="900x500" src="https://source.unsplash.com/J-ClkL7wNUs/900x300" />
     <Carousel.Caption>
-      <h3>First slide label</h3>
+      <h3>Atlanta</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="https://source.unsplash.com/_WuPjE-MPHo/900x500"/>
+    <img width={13000} height={100} alt="900x500" src="https://source.unsplash.com/_WuPjE-MPHo/900x300"/>
     <Carousel.Caption>
-      <h3>Second slide label</h3>
+      <h3>New Delhi</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="https://source.unsplash.com/ubSUwrr04Ks/900x500" />
+    <img width={13000} height={100} alt="900x500" src="https://source.unsplash.com/ubSUwrr04Ks/900x300" />
     <Carousel.Caption>
-      <h3>Third slide label</h3>
+      <h3>New York City</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
