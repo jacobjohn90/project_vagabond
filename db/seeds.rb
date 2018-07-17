1b8b8f9d2daa11d11f11e9aa6d2f5370371abc52
kennf136 <kennf136@gmail.com>
2018-07-17 10:17:18 -0400

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,13 +46,13 @@ City.create(
 ),
 
 Post.create(User_id: 1, City_id:1,:comment=>'There is a lot of fixin ta do here!', :title=>'Can we fix it'),
-Post.create(User_id: 1, City_id:1,:comment=>'Just left from a really important build over there, an its an really great place to move!', :title=>'love this town'),
-Post.create(User_id: 1, City_id:1,:comment=>'Theres a lot of projects going on!',:title=>'So much fun!'),
+Post.create(User_id: 1, City_id:2,:comment=>'Just left from a really important build over there, an its an really great place to move!', :title=>'love this town'),
+Post.create(User_id: 1, City_id:3,:comment=>'Theres a lot of projects going on!',:title=>'So much fun!'),
 
-Post.create(User_id: 2, City_id:2,:comment=>'WOW,ThEREs ALSORTS Of COOL STUFF TO SEE AND EXPLORe',:title=>'Blue skidoo we can too'),
+Post.create(User_id: 2, City_id:1,:comment=>'WOW,ThEREs ALSORTS Of COOL STUFF TO SEE AND EXPLORe',:title=>'Blue skidoo we can too'),
 Post.create(User_id: 2, City_id:2,:comment=>'There are paw prints EVERYWHERE',:title=>'Where is blue?!'),
-Post.create(User_id: 2, City_id:2,:comment=>'Our frind Sally is from here! Lets see what she did today!',:title=>'ITS THE MAIL!'),
+Post.create(User_id: 2, City_id:3,:comment=>'Our frind Sally is from here! Lets see what she did today!',:title=>'ITS THE MAIL!'),
 
-Post.create(User_id: 3, City_id:3,:comment=>'A Woman To raise At a Young age No help From him But she is alive To be Made strong', :title=>'Shes alive'),
-Post.create(User_id: 3, City_id:3,:comment=>'Many a day has passed, the night has gone by, But still I find the time to put that bump off in your eye', :title=>'Hold on now!'),
+Post.create(User_id: 3, City_id:1,:comment=>'A Woman To raise At a Young age No help From him But she is alive To be Made strong', :title=>'Shes alive'),
+Post.create(User_id: 3, City_id:2,:comment=>'Many a day has passed, the night has gone by, But still I find the time to put that bump off in your eye', :title=>'Hold on now!'),
 Post.create(User_id: 3, City_id:3,:comment=>'Walk it out like an usher If you say real talk, I probably wont trust ya If you want to go to war, the guns my pleasure Even Jesus had twelve disciples on the lever, trigger, whatever',:title=>'walk it out'),
