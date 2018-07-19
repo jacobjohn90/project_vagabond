005632a5bbdaa359e3b3c9a3ba239dbf584743b5
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-19 14:36:21 -0400

    added validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :city
+  validates :title, presence: true { message: "But have a title" }
+  validates :comment, length: { in: 1..200} { message: "Comment can only be 1-200 characters long" }
 end
